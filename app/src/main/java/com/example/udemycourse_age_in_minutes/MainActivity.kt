e46a27ebb0c28c37671599f666a99314328e0a62
Ryuji Oda <ryuji.oda@gmail.com>
2021-02-08 23:38:50 +0900

    wip button cliscklistener

--- app/src/main/java/com/example/udemycourse_age_in_minutes/MainActivity.kt
+++ app/src/main/java/com/example/udemycourse_age_in_minutes/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.udemycourse_age_in_minutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDatePicker.setOnClickListener {view ->
+            clickDatePicker(view)
+            Toast.makeText(this, "Button works", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun clickDatePicker(view: View) {
+
     }
 }
