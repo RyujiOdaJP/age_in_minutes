8109cd5f4202013a1d0d17cb232f02e46c658e05
Ryuji Oda <ryuji.oda@gmail.com>
2021-02-08 10:12:16 +0900

    init & wip layout

---
+++ app/src/main/java/com/example/udemycourse_age_in_minutes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.udemycourse_age_in_minutes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
